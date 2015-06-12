02215570df6709ccc72be5eca47485fc42b376d3
Frank Pinter <frank.pinter@brattle.com>
2015-06-11 18:02:08 -0700

    simplify C++ code

--- src/geohashr.cpp
+++ src/geohashr.cpp
@@ -14,12 +14,12 @@ Rcpp::CharacterVector cpp_geohash_encode(Rcpp::NumericVector lat,
 
   Rcpp::NumericVector::iterator it_lat, it_lng;
   Rcpp::CharacterVector::iterator it_out;
+
   for(it_lat = lat.begin(), it_lng = lng.begin(), it_out = geohash_out.begin();
       it_lat != lat.end() && it_lng != lng.end() && it_out != geohash_out.end();
       ++it_lat, ++it_lng, ++it_out) {
-    char *geohash = geohash_encode(*it_lat, *it_lng, precision);
-    Rcpp::String curr_hash(geohash);
-    *it_out = curr_hash;
+
+    *it_out = geohash_encode(*it_lat, *it_lng, precision);
   }
   return geohash_out;
 }
@@ -33,11 +33,14 @@ Rcpp::DataFrame cpp_geohash_decode(Rcpp::CharacterVector hash) {
 
   Rcpp::CharacterVector::iterator it_hash;
   Rcpp::NumericVector::iterator it_lat, it_lng;
+
+  GeoCoord decoded_hash;
+
   for(it_hash = hash.begin(), it_lat = lat.begin(), it_lng = lng.begin();
       it_hash != hash.end() && it_lat != lat.end() && it_lng != lng.end();
       ++it_hash, ++it_lat, ++it_lng) {
-    char *hash0 = *it_hash;
-    GeoCoord decoded_hash = geohash_decode(hash0);
+
+    decoded_hash = geohash_decode(*it_hash);
     *it_lat = decoded_hash.latitude;
     *it_lng = decoded_hash.longitude;
   }
